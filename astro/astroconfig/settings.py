9ee3a41d3ebc85d3dcf1e2009bb0b029f781bddb
CorneliaGit <tepi.djet.neheh@gmail.comcd>
2024-10-23 12:11:57 +0800

    Intégration de la vue wheeldisplay dans birth_results

--- astro/astroconfig/settings.py
+++ astro/astroconfig/settings.py
@@ -101,7 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Dossier pour les fichiers statiques (images, CSS, etc.)
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'astroapp', 'static')]
