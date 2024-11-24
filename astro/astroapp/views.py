e1db89e235f1b4a4569034527acc4ffd2c30eed3
CorneliaGit <tepi.djet.neheh@gmail.comcd>
2024-11-24 16:13:40 +0800

    Ajout des fonctions liées aux symboles zodiacaux dans astroapp/wheel/wheel_symbols.py, incluant ,  et

--- astro/astroapp/views.py
+++ astro/astroapp/views.py
@@ -87,15 +87,21 @@ from astroapp.calculs.ephemeris_calculations import (
     calculate_julian_day_and_planet_positions,
     calculate_julian_and_positions,
 )
+from astroapp.calculs.houses_calculations import (
+    format_house_cusp,
+    get_asc_mc,
+    calculate_houses,
+    calculate_astrological_houses,
+    calculate_positions_and_houses,
+)
 
 
-from astroapp.calculs.houses_calculations import format_house_cusp
-from astroapp.calculs.houses_calculations import get_asc_mc
-from astroapp.calculs.houses_calculations import calculate_houses
-from astroapp.calculs.houses_calculations import calculate_astrological_houses
-from astroapp.calculs.houses_calculations import calculate_positions_and_houses
 
 
+
+from astroapp.wheel.wheel_symbols import draw_zodiac_symbols
+from astroapp.wheel.wheel_symbols import get_planet_colors
+from astroapp.wheel.wheel_symbols import display_planet_degrees
 
 
 
@@ -564,7 +570,6 @@ def planetary_position(request):# Calculer le jour julien (JD)
     
 # ROUE
 
-
 # RESTE DANS VIEWS
 def prepare_aspects_text(aspects, planet_positions):
     """Prépare le texte formaté des aspects pour l'affichage."""
@@ -735,36 +740,7 @@ def draw_divisions(ax, rotation_offset):
 
 
 
-def draw_zodiac_symbols(ax, rotation_offset):
-    # Appel de la fonciton pour associer les éléments aux signes du zodiaque : def get_zodiac_data
-    elements, sign_colors, zodiac_symbols = get_zodiac_data()
 
-
-    # Liste des symboles zodiacaux (en utilisant des symboles unicode ou police spécifique)
-    zodiac_symbols = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'v', 'x', 'c']
-    
-    # Appel de la fonciton pour Charger la police HamburgSymbols si nécessaire : def load_zodiac_font
-    prop = load_zodiac_font()
-
-
-    # Placer chaque symbole du zodiaque
-    for i, symbol in enumerate(zodiac_symbols):
-        # Calculer l'angle de positionnement pour chaque signe
-        angle = np.radians(30 * i + 15)
-        angle_corrected = angle + rotation_offset
-        x_text = 0.96 * np.cos(angle_corrected)
-        y_text = 0.96 * np.sin(angle_corrected)
-
-        # Déterminer la couleur du signe en fonction de son élément
-        element = elements[i]
-        symbol_color = sign_colors[element]
-
-        # Définir la taille du symbole zodiacal en fonction de la figure
-        symbol_size = 40  # Ajustable si nécessaire
-
-        # Ajouter le symbole à la figure
-        ax.text(x_text, y_text, symbol, fontsize=symbol_size, 
-                ha='center', va='center', fontproperties=prop, color=symbol_color)
 
 
 
@@ -813,36 +789,10 @@ def draw_houses_and_cusps(ax, house_results, rotation_offset):
     add_house_triangles(ax, house_results, rotation_offset)
 
 
-def get_planet_colors():
-    """Retourne le dictionnaire des couleurs pour chaque planète."""
-    return {
-        'Soleil': '#FFA801', 'Lune': '#FFA801', 'Mercure': '#A9CE02', 'Vénus': '#BB55A1',
-        'Mars': '#F9074C', 'Jupiter': '#07B3F2', 'Saturne': '#6A3D90', 
-        'Uranus': '#9B207B', 'Neptune': '#07B3F2', 'Pluton': '#3B2B7B'
-    }
 
 
-def display_planet_degrees(ax, planet_positions, rotation_offset, planet_colors):
-    """Affiche les degrés et minutes pour chaque planète sur la roue astrologique."""
-    for planet, degree in planet_positions:
-        angle = np.radians(degree)
-        angle_corrected = angle + rotation_offset
 
-        degree_in_sign = degree % 30
-        minutes = int((degree_in_sign - int(degree_in_sign)) * 60)
 
-        x_degree = 1.94 * np.cos(angle_corrected)
-        y_degree = 1.94 * np.sin(angle_corrected)
-        
-        x_minutes = 2.10 * np.cos(angle_corrected)
-        y_minutes = 2.10 * np.sin(angle_corrected)
-        
-        degree_color = planet_colors.get(planet, 'black')
-
-        ax.text(x_degree, y_degree, f"{int(degree_in_sign)}°", fontsize=13, 
-                ha='center', va='center', color=degree_color, weight='bold')
-        ax.text(x_minutes, y_minutes, f"{minutes}'", fontsize=11, 
-                ha='center', va='center', color=degree_color, weight='bold')
 
 
 def display_house_degrees(ax, house_results, rotation_offset):
--- astro/astroapp/wheel/wheel_symbols.py
+++ astro/astroapp/wheel/wheel_symbols.py
@@ -1,2 +1,72 @@
+import numpy as np
+from astroapp.utils.zodiac_utils import get_zodiac_data, load_zodiac_font
+
+def draw_zodiac_symbols(ax, rotation_offset):
+    # Appel de la fonciton pour associer les éléments aux signes du zodiaque : def get_zodiac_data
+    elements, sign_colors, zodiac_symbols = get_zodiac_data()
+
+
+    # Liste des symboles zodiacaux (en utilisant des symboles unicode ou police spécifique)
+    zodiac_symbols = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'v', 'x', 'c']
+    
+    # Appel de la fonciton pour Charger la police HamburgSymbols si nécessaire : def load_zodiac_font
+    prop = load_zodiac_font()
+
+
+    # Placer chaque symbole du zodiaque
+    for i, symbol in enumerate(zodiac_symbols):
+        # Calculer l'angle de positionnement pour chaque signe
+        angle = np.radians(30 * i + 15)
+        angle_corrected = angle + rotation_offset
+        x_text = 0.96 * np.cos(angle_corrected)
+        y_text = 0.96 * np.sin(angle_corrected)
+
+        # Déterminer la couleur du signe en fonction de son élément
+        element = elements[i]
+        symbol_color = sign_colors[element]
+
+        # Définir la taille du symbole zodiacal en fonction de la figure
+        symbol_size = 40  # Ajustable si nécessaire
+
+        # Ajouter le symbole à la figure
+        ax.text(x_text, y_text, symbol, fontsize=symbol_size, 
+                ha='center', va='center', fontproperties=prop, color=symbol_color)
+
+
+
+
+def get_planet_colors():
+    """Retourne le dictionnaire des couleurs pour chaque planète."""
+    return {
+        'Soleil': '#FFA801', 'Lune': '#FFA801', 'Mercure': '#A9CE02', 'Vénus': '#BB55A1',
+        'Mars': '#F9074C', 'Jupiter': '#07B3F2', 'Saturne': '#6A3D90', 
+        'Uranus': '#9B207B', 'Neptune': '#07B3F2', 'Pluton': '#3B2B7B'
+    }
+    
+    
+def display_planet_degrees(ax, planet_positions, rotation_offset, planet_colors):
+    """Affiche les degrés et minutes pour chaque planète sur la roue astrologique."""
+    for planet, degree in planet_positions:
+        angle = np.radians(degree)
+        angle_corrected = angle + rotation_offset
+
+        degree_in_sign = degree % 30
+        minutes = int((degree_in_sign - int(degree_in_sign)) * 60)
+
+        x_degree = 1.94 * np.cos(angle_corrected)
+        y_degree = 1.94 * np.sin(angle_corrected)
+        
+        x_minutes = 2.10 * np.cos(angle_corrected)
+        y_minutes = 2.10 * np.sin(angle_corrected)
+        
+        degree_color = planet_colors.get(planet, 'black')
+
+        ax.text(x_degree, y_degree, f"{int(degree_in_sign)}°", fontsize=13, 
+                ha='center', va='center', color=degree_color, weight='bold')
+        ax.text(x_minutes, y_minutes, f"{minutes}'", fontsize=11, 
+                ha='center', va='center', color=degree_color, weight='bold')
+    
+    
+    
 
 
