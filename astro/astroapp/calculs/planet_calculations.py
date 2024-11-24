86fbf5c80a4aa97afec9b8d82e4830076cd9f496
CorneliaGit <tepi.djet.neheh@gmail.comcd>
2024-11-24 11:36:58 +0800

    Déplacement des fonctions calculate_planet_positions et format_planet_positions dans astroapp/calculs/planet_calculations.py, résolution du problème lié à l'enregistrement et au son

--- astro/astroapp/calculs/planet_calculations.py
+++ astro/astroapp/calculs/planet_calculations.py
@@ -1,2 +1,62 @@
+import swisseph as swe
+from astroapp.utils.zodiac_utils import get_zodiac_sign
 
+
+def calculate_single_planet_position(jd, planet_id):
+    """Calcule la position d'une planète en fonction du jour julien."""
+    position, _ = swe.calc_ut(jd, planet_id)
+    degree = position[0]
+    sign, sign_degree = get_zodiac_sign(degree)
+
+    # Afficher la position pour débogage
+    print(f"Débogage : Planète ID {planet_id} - Degré :", degree, ", Signe :", sign, ", Degré dans le signe :", sign_degree)
+
+    planet_data = {
+        'degree': degree,
+        'sign': sign,
+        'sign_degree': sign_degree,
+    }
+    return planet_data, degree
+    
+    
+    
+# Fonction pour calculer les positions des planètes
+def calculate_planet_positions(jd):
+    planets = {
+        'Soleil': swe.SUN,
+        'Lune': swe.MOON,
+        'Mercure': swe.MERCURY,
+        'Vénus': swe.VENUS,
+        'Mars': swe.MARS,
+        'Jupiter': swe.JUPITER,
+        'Saturne': swe.SATURN,
+        'Uranus': swe.URANUS,
+        'Neptune': swe.NEPTUNE,
+        'Pluton': swe.PLUTO,
+    }
+    results = {}
+    planet_positions = []
+
+    print("Débogage : Calcul des positions des planètes pour le jour julien (JD) ->", jd)
+
+
+    for planet_name, planet_id in planets.items():
+        # Appel de la fonction pour calculer la position d'une planète : def calculate_single_planet_position
+        planet_data, planet_degree = calculate_single_planet_position(jd, planet_id)
+        results[planet_name] = planet_data
+        planet_positions.append((planet_name, planet_degree))
+
+
+    # Appel de la fonction pour formater les résultats avant de les renvoyer : def format_planet_positions(
+    return format_planet_positions(results, planet_positions)
+
+
+
+
+def format_planet_positions(results, planet_positions):
+    """Prépare les résultats des positions planétaires pour le retour."""
+    # Affiche les résultats pour vérification
+    print("Débogage : Résultats des positions des planètes ->", results)
+    print("Débogage : Positions des planètes en liste ->", planet_positions)
+    return results, planet_positions
 
--- astro/astroapp/views.py
+++ astro/astroapp/views.py
@@ -29,6 +29,7 @@ import json
 from django.utils.http import urlencode
 from django.http import JsonResponse
 from django.contrib import messages  # Import pour les messages flash
+
 from astroapp.utils.geolocation_utils import (
     get_location,
     get_timezone,
@@ -61,24 +62,27 @@ from astroapp.utils.planet_utils import (
     place_planet,
     draw_planet_positions
 )
+from astroapp.utils.data_utils import (
+    format_single_aspect,
+    prepare_theme_data_json,
+    prepare_wheel_context,
+    prepare_planetary_context,
+    extract_request_parameters,
+    extract_wheel_data,
+    deserialize_wheel_data,
+)
+from astroapp.calculs.aspects_calculations import (
+    calculate_angular_difference,
+    add_aspect_if_present,
+    calculate_aspects,
+    calculate_astrological_aspects,
+)
 
-from astroapp.utils.data_utils import format_single_aspect
-from astroapp.utils.data_utils import prepare_theme_data_json
-from astroapp.utils.data_utils import prepare_wheel_context
-from astroapp.utils.data_utils import prepare_planetary_context
-from astroapp.utils.data_utils import extract_request_parameters
-from astroapp.utils.data_utils import extract_wheel_data
-from astroapp.utils.data_utils import deserialize_wheel_data
+from astroapp.calculs.planet_calculations import calculate_single_planet_position
 
+from astroapp.calculs.planet_calculations import calculate_planet_positions
 
-
-
-
-from astroapp.calculs.aspects_calculations import calculate_angular_difference
-from astroapp.calculs.aspects_calculations import add_aspect_if_present
-from astroapp.calculs.aspects_calculations import calculate_aspects
-from astroapp.calculs.aspects_calculations import calculate_astrological_aspects
-
+from astroapp.calculs.planet_calculations import format_planet_positions
 
 
 
@@ -263,61 +267,13 @@ def calculate_julian_and_positions(birth_datetime_utc):
 
 
 # PLANETES 
-def calculate_single_planet_position(jd, planet_id):
-    """Calcule la position d'une planète en fonction du jour julien."""
-    position, _ = swe.calc_ut(jd, planet_id)
-    degree = position[0]
-    sign, sign_degree = get_zodiac_sign(degree)
-
-    # Afficher la position pour débogage
-    print(f"Débogage : Planète ID {planet_id} - Degré :", degree, ", Signe :", sign, ", Degré dans le signe :", sign_degree)
-
-    planet_data = {
-        'degree': degree,
-        'sign': sign,
-        'sign_degree': sign_degree,
-    }
-    return planet_data, degree
 
 
-def format_planet_positions(results, planet_positions):
-    """Prépare les résultats des positions planétaires pour le retour."""
-    # Affiche les résultats pour vérification
-    print("Débogage : Résultats des positions des planètes ->", results)
-    print("Débogage : Positions des planètes en liste ->", planet_positions)
-    return results, planet_positions
 
 
-# Fonction pour calculer les positions des planètes
-def calculate_planet_positions(jd):
-    planets = {
-        'Soleil': swe.SUN,
-        'Lune': swe.MOON,
-        'Mercure': swe.MERCURY,
-        'Vénus': swe.VENUS,
-        'Mars': swe.MARS,
-        'Jupiter': swe.JUPITER,
-        'Saturne': swe.SATURN,
-        'Uranus': swe.URANUS,
-        'Neptune': swe.NEPTUNE,
-        'Pluton': swe.PLUTO,
-    }
-    results = {}
-    planet_positions = []
-
-    print("Débogage : Calcul des positions des planètes pour le jour julien (JD) ->", jd)
 
 
-    for planet_name, planet_id in planets.items():
-        # Appel de la fonction pour calculer la position d'une planète : def calculate_single_planet_position
-        planet_data, planet_degree = calculate_single_planet_position(jd, planet_id)
-        results[planet_name] = planet_data
-        planet_positions.append((planet_name, planet_degree))
 
-
-    # Appel de la fonction pour formater les résultats avant de les renvoyer : def format_planet_positions(
-    return format_planet_positions(results, planet_positions)
-    
     
 
 
