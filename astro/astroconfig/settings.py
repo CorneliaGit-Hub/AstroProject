ad8dcca72fa51414b64125170289d743ed065af2
CorneliaGit <tepi.djet.neheh@gmail.comcd>
2024-11-30 15:33:08 +0800

    Suppression du formulaire dans wheeldisplay.html

--- astro/astroconfig/settings.py
+++ astro/astroconfig/settings.py
@@ -99,6 +99,13 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGE_CODE = 'fr'
+TIME_ZONE = 'Europe/Paris'
+
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
